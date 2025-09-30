13533fd7d063725cd58775b547f04e23c2f167e0
Ethan J <ethan@yasfu.net>
2025-09-29 21:17:37 -0600

    fix: resource pack not returning correctly all the time

--- src/main/java/me/spartacus04/jext/AssetsManager.kt
+++ src/main/java/me/spartacus04/jext/AssetsManager.kt
@@ -64,6 +64,7 @@ class AssetsManager {
                 null
             }
         } else {
+            Bukkit.getLogger().warning("No resource pack specified or found. " + CONFIG.RESOURCE_PACK_URL + " " + CONFIG.RESOURCE_PACK_HASH + " " + resourcePack);
             null
         }
     }
@@ -202,9 +203,7 @@ class AssetsManager {
 
     private val resourcePack: String
         get() = try {
-            if (CONFIG.RESOURCE_PACK_URL.isNotBlank())
-                CONFIG.RESOURCE_PACK_URL
-            Bukkit.getServer().resourcePack
+            CONFIG.RESOURCE_PACK_URL.ifBlank { Bukkit.getServer().resourcePack }
         } catch (_: NoSuchMethodError) {
             val propertiesFile = File(".").resolve("server.properties")
 
@@ -215,7 +214,7 @@ class AssetsManager {
         get() = try {
             if (CONFIG.RESOURCE_PACK_URL.isNotBlank())
                 CONFIG.RESOURCE_PACK_HASH
-            Bukkit.getServer().resourcePackHash
+            else Bukkit.getServer().resourcePackHash
         } catch (_: NoSuchMethodError) {
             val propertiesFile = File(".").resolve("server.properties")
 
